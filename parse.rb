af9bf5cc812f9b833a1be66ca2abfe1b1b33f7c1
mozukraken <mozukraken@gmail.com>
2018-06-12 22:16:09 +0900

    複数のデータを読み込んでみた

--- parse.rb
+++ parse.rb
@@ -1,11 +1,17 @@
-emails = [
-  "test1@example.com",
-  "test2@example.com"
-]
+# emails = [
+#  "test1@example.com",
+#  "test2@example.com"
+# ]
 
 require 'yaml'
 
-emails_yaml = YAML.load_file('mydata.yml')
+# emails_yaml = YAML.load_file('mydata.yml')
 
-p emails
-p emails_yaml
+# p emails
+# p emails_yaml
+
+File.open('mydata.yml') do |f|
+  YAML.load_documents(f) do |d|
+    p d
+  end
+end
